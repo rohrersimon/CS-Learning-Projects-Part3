3092b8529ec1a05f67af78a1728792322196913f
Simon Rohrer <simon.rohrer@live.com>
2023-11-03 08:05:48 +0000

    commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,33 @@
-﻿string[] ipv4Input = {"107.31.1.5", "255.0.0.255", "555..0.555", "255...255"};
+﻿
+
+//#############################################################
+
+/*
+Random random = new Random();
+int luck = random.Next(100);
+
+tellFortune(luck);
+
+void tellFortune(int luck)
+{
+    string[] text = { "You have much to", "Today is a day to", "Whatever work you do", "This is an ideal time to" };
+    string[] good = { "look forward to.", "try new things!", "is likely to succeed.", "accomplish your dreams!" };
+    string[] bad = { "fear.", "avoid major decisions.", "may have unexpected outcomes.", "re-evaluate your life." };
+    string[] neutral = { "appreciate.", "enjoy time with friends.", "should align with your values.", "get in tune with nature." };
+
+    Console.WriteLine("A fortune teller whispers the following words:");
+    string[] fortune = (luck > 75 ? good : (luck < 25 ? bad : neutral));
+    for (int i = 0; i < 4; i++)
+    {
+        Console.Write($"{text[i]} {fortune[i]} ");
+    }
+}
+*/
+
+//#############################################################
+
+/*
+string[] ipv4Input = {"107.31.1.5", "255.0.0.255", "555..0.555", "255...255"};
 string[] address;
 bool validLength = false;
 bool validZeroes = false;
@@ -54,3 +83,4 @@ void ValidateRange()
     }
     validRange = true;
 }
+*/
